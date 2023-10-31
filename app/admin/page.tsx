7bd90794a10e227d55d4768d89306dcc3baf074e
Zakaria Ait Ali <zakaria.aitali@etu.uae.ac.ma>
2023-10-31 20:25:38 +0100

    using /app routing

---
+++ app/admin/page.tsx
@@ -0,0 +1,17 @@
+export default async function AdminPage() {
+
+    return (
+        <div
+            style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '100vh',
+                width: '100vw',
+                backgroundColor: '#f0f0f0',
+            }}
+        >
+            <h1>admin</h1>
+        </div>
+    );
+}
