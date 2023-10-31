9fb5cb5cee525aca9371c6bc1d41616550a4aae4
Zakaria Ait Ali <zakaria.aitali@etu.uae.ac.ma>
2023-10-31 22:09:02 +0100

    nextUI next-themes setup

---
+++ app/providers.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import { NextUIProvider } from '@nextui-org/react'
+import { ThemeProvider as NextThemesProvider } from 'next-themes'
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+    return (
+        <NextUIProvider>
+            <NextThemesProvider
+                attribute='class'
+                defaultTheme='dark'
+                themes={['light', 'dark', 'modern']}
+            >
+                {children}
+            </NextThemesProvider>
+        </NextUIProvider>
+    )
+}
