caae4603d05ba0e1f1d9dabd25b8d9720447cffd
Zakaria Ait Ali <zakaria.aitali@etu.uae.ac.ma>
2023-11-02 19:43:59 +0100

    simple full calendar

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,66 +1,24 @@
 'use client'
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef } from "react";
 import FullCalendar from "@fullcalendar/react";
 import interactionPlugin from "@fullcalendar/interaction";
 import timeGridPlugin from "@fullcalendar/timegrid";
-
+import dayGridPlugin from "@fullcalendar/daygrid";
 const Calendar = () => {
-    const calendarRef = useRef<FullCalendar>(null);
-    const [isModalOpen, setModalOpen] = useState(false);
-    const [taskTitle, setTaskTitle] = useState("");
-
-    const handleDateClick = (info: { date: Date; allDay: boolean }) => {
-        const calendarApi = calendarRef.current?.getApi();
-
-        if (calendarApi) {
-            setModalOpen(true);
-        }
-    };
-
-    const saveTask = () => {
-        const calendarApi = calendarRef.current?.getApi();
-
-        if (calendarApi && taskTitle.trim() !== "") {
-            // Add task to the calendar
-            calendarApi.addEvent({
-                title: taskTitle,
-                start: new Date( Date.now() ),
-                allDay: false, // Set to false or true based on your requirement
-            });
-
-            // Close the modal and reset taskTitle
-            setModalOpen(false);
-            setTaskTitle("");
-        }
-    };
-
     return (
         <>
             <FullCalendar
-                ref={calendarRef}
-                plugins={[timeGridPlugin, interactionPlugin]}
-                editable
-                selectable
-                selectMirror
-                dateClick={handleDateClick}
+                plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+                initialView={"timeGridWeek"}
+                headerToolbar={
+                    {
+                        right: "prev,next today",
+                        center: "title",
+                        left: "dayGridMonth,timeGridWeek,timeGridDay",
+                    }
+                }
+                height={"auto"}
             />
-
-            {/* Render your custom modal element */}
-            {isModalOpen && (
-
-                <div className="custom-modal-overlay">
-                    <div className="custom-modal">
-                        <label>Task Title:</label>
-                        <input
-                            type="text"
-                            value={taskTitle}
-                            onChange={(e) => setTaskTitle(e.target.value)}
-                        />
-                        <button onClick={saveTask}>Save</button>
-                        <button onClick={() => setModalOpen(false)}>Cancel</button>
-                    </div>
-                </div>
-            )}
         </>
     );
 };
