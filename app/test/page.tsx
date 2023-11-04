f9252bb2eb01733ce95e800b8673026daa4a2c51
Zakaria Ait Ali <zakaria.aitali@etu.uae.ac.ma>
2023-11-04 11:13:42 +0100

    navbar + fix in calendar

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,15 +1,15 @@
 "use client"
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
-import interactionPlugin, { Draggable, DropArg } from '@fullcalendar/interaction'
+import interactionPlugin, {Draggable, DropArg} from '@fullcalendar/interaction'
 import timeGridPlugin from '@fullcalendar/timegrid'
-import { Fragment, useEffect, useState } from 'react'
-import { Dialog, Transition } from '@headlessui/react'
-import { CheckIcon, ExclamationTriangleIcon } from '@heroicons/react/20/solid'
-import { EventSourceInput } from '@fullcalendar/core/index.js'
+import {Fragment, useEffect, useState} from 'react'
+import {Dialog, Transition} from '@headlessui/react'
+import {CheckIcon, ExclamationTriangleIcon} from '@heroicons/react/20/solid'
+import {EventSourceInput} from '@fullcalendar/core/index.js'
 import axios from 'axios';
-import { useRouter } from "next/navigation";
-import { set } from 'animejs'
+import {useRouter} from "next/navigation";
+import {set} from 'animejs'
 
 
 interface Event {
@@ -21,11 +21,11 @@ interface Event {
 
 export default function Home() {
     const [events, setEvents] = useState([
-        { title: 'event 1', id: '1' },
-        { title: 'event 2', id: '2' },
-        { title: 'event 3', id: '3' },
-        { title: 'event 4', id: '4' },
-        { title: 'event 5', id: '5' },
+        {title: 'event 1', id: '1'},
+        {title: 'event 2', id: '2'},
+        {title: 'event 3', id: '3'},
+        {title: 'event 4', id: '4'},
+        {title: 'event 5', id: '5'},
     ])
     const [allEvents, setAllEvents] = useState<Event[]>([])
     const [showModal, setShowModal] = useState(false)
@@ -43,7 +43,7 @@ export default function Home() {
     const [pause, setBreak] = useState("");
     const [error, setError] = useState(false);
     const router = useRouter();
-    const [data, setData] = useState({ isStart: false, isEnd: false });
+    const [data, setData] = useState({isStart: false, isEnd: false});
     const [loading, setLoading] = useState(true);
     const [idEmployee, setIdEmployee] = useState("");
 
@@ -51,12 +51,20 @@ export default function Home() {
     const handleButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
         const buttonValue = event.currentTarget.value;
         console.log('Clicked button value: ' + buttonValue);
-        var time = (typeof newEvent.start === 'string' ? newEvent.start : newEvent.start.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false }));
-        if (buttonValue == "start") { setStartTime(time); }
-        else if (buttonValue == "end") { setEndTime(time); }
-        else if (buttonValue == "pause") { setBreak(time); }
+        var time = (typeof newEvent.start === 'string' ? newEvent.start : newEvent.start.toLocaleTimeString([], {
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit',
+            hour12: false
+        }));
+        if (buttonValue == "start") {
+            setStartTime(time);
+        } else if (buttonValue == "end") {
+            setEndTime(time);
+        } else if (buttonValue == "pause") {
+            setBreak(time);
+        }
     };
-
 
 
     useEffect(() => {
@@ -68,7 +76,7 @@ export default function Home() {
                     let title = eventEl.getAttribute("title")
                     let id = eventEl.getAttribute("data")
                     let start = eventEl.getAttribute("start")
-                    return { title, id, start }
+                    return {title, id, start}
                 }
             })
         }
@@ -125,8 +133,7 @@ export default function Home() {
                 router.push('/test');
             } else if (response.status === 401) {
                 router.push('/login');
-            }
-            else {
+            } else {
 
                 setError(true);
             }
@@ -139,7 +146,7 @@ export default function Home() {
 
 
     function handleDateClick(arg: { date: Date, allDay: boolean }) {
-        setNewEvent({ ...newEvent, start: arg.date, allDay: arg.allDay, id: new Date().getTime() })
+        setNewEvent({...newEvent, start: arg.date, allDay: arg.allDay, id: new Date().getTime()})
 
         console.log("Date Click Data:", arg.date);
         console.log("All Day:", arg.allDay);
@@ -149,7 +156,13 @@ export default function Home() {
     }
 
     function addEvent(data: DropArg) {
-        const event = { ...newEvent, start: data.date.toISOString(), title: data.draggedEl.innerText, allDay: data.allDay, id: new Date().getTime() }
+        const event = {
+            ...newEvent,
+            start: data.date.toISOString(),
+            title: data.draggedEl.innerText,
+            allDay: data.allDay,
+            id: new Date().getTime()
+        }
         setAllEvents([...allEvents, event])
 
         console.log("Dropped Event Data:", event);
@@ -193,11 +206,10 @@ export default function Home() {
     }
 
 
-
     return (
         <>
             <main className="flex min-h-screen flex-col items-center justify-between p-24">
-                <div className="grid grid-cols-10">
+                <div className="grid grid-cols">
                     <div className="col-span-8">
                         <FullCalendar
                             plugins={[
@@ -211,6 +223,7 @@ export default function Home() {
                                 center: 'title',
                                 right: ''
                             }}
+                            firstDay={6}
                             height={"auto"}
                             events={allEvents as EventSourceInput}
                             nowIndicator={true}
@@ -226,18 +239,6 @@ export default function Home() {
                             eventClick={(data) => handleDeleteModal(data)}
                         />
                     </div>
-                    <div id="draggable-el" className="ml-8 w-full border-2 p-2 rounded-md mt-16 lg:h-1/2 bg-violet-50">
-                        <h1 className="font-bold text-lg text-center">Drag Event</h1>
-                        {events.map(event => (
-                            <div
-                                className="fc-event border-2 p-1 m-2 w-full rounded-md ml-auto text-center bg-white"
-                                title={event.title}
-                                key={event.id}
-                            >
-                                {event.title}
-                            </div>
-                        ))}
-                    </div>
                 </div>
 
                 <Transition.Root show={showDeleteModal} as={Fragment}>
@@ -252,7 +253,7 @@ export default function Home() {
                             leaveTo="opacity-0"
 
                         >
-                            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+                            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"/>
                         </Transition.Child>
 
                         <div className="fixed inset-0 z-10 overflow-y-auto">
@@ -275,11 +276,11 @@ export default function Home() {
                                                 <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center
                       justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
                                                     <ExclamationTriangleIcon className="h-6 w-6 text-red-600"
-                                                        aria-hidden="true" />
+                                                                             aria-hidden="true"/>
                                                 </div>
                                                 <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
                                                     <Dialog.Title as="h3"
-                                                        className="text-base font-semibold leading-6 text-gray-900">
+                                                                  className="text-base font-semibold leading-6 text-gray-900">
                                                         Delete Event
                                                     </Dialog.Title>
                                                     <div className="mt-2">
@@ -297,7 +298,7 @@ export default function Home() {
                                             </button>
                                             <button type="button" className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900
                       shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
-                                                onClick={handleCloseModal}
+                                                    onClick={handleCloseModal}
                                             >
                                                 Cancel
                                             </button>
@@ -319,7 +320,7 @@ export default function Home() {
                             leaveFrom="opacity-100"
                             leaveTo="opacity-0"
                         >
-                            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+                            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"/>
                         </Transition.Child>
 
                         <div className="fixed inset-0 z-10 overflow-y-auto">
@@ -339,65 +340,43 @@ export default function Home() {
                                         <div>
                                             <div
                                                 className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100">
-                                                <CheckIcon className="h-6 w-6 text-green-600" aria-hidden="true" />
+                                                <CheckIcon className="h-6 w-6 text-green-600" aria-hidden="true"/>
                                             </div>
                                             <div className="mt-3 text-center sm:mt-5">
                                                 <Dialog.Title as="h3"
-                                                    className="text-base font-semibold leading-6 text-gray-900">
+                                                              className="text-base font-semibold leading-6 text-gray-900">
                                                     Add Event
                                                 </Dialog.Title>
                                                 <form action="submit" onSubmit={handleSubmit} method="post">
-                                                    <div className="mt-2">
-                                                        <input type="text" name="title" className="block w-full rounded-md border-0 py-1.5 text-gray-900
-                            shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 
-                            focus:ring-2 
-                            focus:ring-inset focus:ring-violet-600 
-                            sm:text-sm sm:leading-6"
-                                                            value={newEvent.title} onChange={(e) => handleChange(e)}
-                                                            placeholder="Title" />
-                                                    </div>
-                                                    <div
-                                                        className="mt-5 sm:mt-6 sm:grid sm:grid-flow-row-dense sm:grid-cols-2 sm:gap-3">
-
-
+                                                    <div className="mt-5 sm:mt-6 sm:grid sm:grid-flow-row-dense sm:grid-cols-3 sm:gap-3">
                                                         <button
                                                             name="start"
                                                             value="start"
                                                             type="submit"
-                                                            className="inline-flex w-full justify-center rounded-md bg-violet-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-violet-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-violet-600 sm:col-start-2 disabled:opacity-25"
+                                                            className="inline-flex bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 items-center justify-between w-full p-5 text-white-500 border border-gray-200 rounded-lg cursor-pointer dark:hover:text-black-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700 disabled:opacity-20"
                                                             disabled={data.isStart}
                                                             onClick={handleButtonClick}>
                                                             Start of Work
                                                         </button>
-
-                                                        <button
-                                                            type="submit"
-                                                            name="end"
-                                                            value="end"
-                                                            className="inline-flex w-full justify-center rounded-md bg-violet-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-violet-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-violet-600 sm:col-start-2 disabled:opacity-25"
-                                                            disabled={data.isEnd || (!data.isEnd && !data.isStart)}
-                                                            onClick={handleButtonClick}
-                                                        >
-                                                            End of Work
-                                                        </button>
-
                                                         <button
                                                             type="submit"
                                                             name="pause"
                                                             value="pause"
-                                                            className="inline-flex w-full justify-center rounded-md bg-violet-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-violet-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-violet-600 sm:col-start-2 disabled:opacity-25"
+                                                            className="inline-flex bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 items-center justify-between w-full p-5 text-white-500 border border-gray-200 rounded-lg cursor-pointer dark:hover:text-black-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700 disabled:opacity-20"
                                                             disabled={!data.isStart}
                                                             onClick={handleButtonClick}
                                                         >
                                                             Pause
                                                         </button>
                                                         <button
-                                                            type="button"
-                                                            className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:col-start-1 sm:mt-0"
-                                                            onClick={handleCloseModal}
-
+                                                            type="submit"
+                                                            name="end"
+                                                            value="end"
+                                                            className="inline-flex bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 items-center justify-between w-full p-5 text-black-500 border border-gray-200 rounded-lg cursor-pointer dark:hover:text-black-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700 disabled:opacity-20"
+                                                            disabled={data.isEnd || (!data.isEnd && !data.isStart)}
+                                                            onClick={handleButtonClick}
                                                         >
-                                                            Cancel
+                                                            End of Work
                                                         </button>
                                                     </div>
                                                 </form>
